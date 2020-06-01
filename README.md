cc5478f2a56bb01a1560740d2197895887c7b36e
Thomas Cron <thomas.cron@gmail.com>
2020-06-01 11:35:03 +0200

    Ajout du Javascript pour la gestion des mots de passe de la page inscription

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 Thomas CRON
 ## Netlify
 https://connexion-thomascron.netlify.app/
+### Identifiants pour la connexion
+Email : google@gmail.com
+Mot de passe : motdepasse
 ## Maquette Adobe XD
 https://xd.adobe.com/view/71b43dd2-eb3c-4083-5abb-62248a16fd6e-0785/
--- src/index.js
+++ src/index.js
@@ -33,4 +33,71 @@ $(document).ready(function() {
         }
     });
 
+    $("#inscription").submit(function(e){
+        e.preventDefault();
+        alert('Inscription réussie !');
+
+    });
+
+    $('#password_verif').keyup(function(e){
+        checkPassword($('#password_verif').val());
+    })
+    function checkPassword(password2){
+        if(password2 == $("#password_inscri").val()){
+            $('#unverif').removeClass();
+            $('#verif').addClass("display");
+            $('#password_verif').removeClass()
+            $('#password_verif').addClass('input_strong')
+        }else{
+            $('#verif').removeClass();
+            $('#unverif').addClass("display");
+            $('#password_verif').removeClass()
+            $('#password_verif').addClass('input_short')
+        }
+    }
+
+
+    $('#password_inscri').keyup(function() {
+        $('#unsecure').html(checkStrength($('#password_inscri').val()))
+        })
+        function checkStrength(password) {
+        var strength = 0
+        if (password.length < 6) {
+        $('#unsecure').removeClass()
+        $('#unsecure').addClass('short')
+        $('#password_inscri').removeClass()
+        $('#password_inscri').addClass('input_short')
+        return 'Votre mot de passe est trop court. Utilisez au moins 7 caractères.'
+        }
+        if (password.length > 7) {
+            strength += 1
+            $('#unsecure').removeClass()
+            $('#password_inscri').removeClass()
+
+        }
+        if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1
+        if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) strength += 1
+        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+      
+        if (strength < 3) {
+        $('#unsecure').removeClass()
+        $('#unsecure').addClass('weak')
+        $('#password_inscri').removeClass()
+        $('#password_inscri').addClass('input_weak')
+        return "Votre mot de passe n'est pas sécurisé, utilisez au moins, une majuscule, une minuscule, un chiffre et un symbole."
+        } else if (strength == 3) {
+        $('#unsecure').removeClass()
+        $('#unsecure').addClass('good')
+        $('#password_inscri').removeClass()
+        $('#password_inscri').addClass('input_good')
+        return "Votre mot de passe est correct mais peut être encore meilleur, utilisez au moins, une majuscule, une minuscule, un chiffre et un symbole."
+        } else {
+        $('#unsecure').removeClass()
+        $('#unsecure').addClass('strong')
+        $('#password_inscri').removeClass()
+        $('#password_inscri').addClass('input_strong')
+        return 'Parfait ! Votre mot de passe est sécurisé.'
+        }
+        }
+
 });
